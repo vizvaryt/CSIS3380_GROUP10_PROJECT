446b4edad68d687d611446ac13b9bc3b70719ea5
Sanmeetkaur276 <101531186+Sanmeetkaur276@users.noreply.github.com>
2023-04-20 16:31:33 -0700

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import { Routes, Route } from 'react-router-dom';
+import {Routes, Route } from 'react-router-dom';
 
+import Login from './Login';
+import Register from './Register';
 import Home from './Home';
 import AppointmentList from './Appointment-list';
 import AddAppointment from './Add';
@@ -12,16 +14,23 @@ import UpdateAppointment from './Update';
 const App = () => (
     <div className="container">
       <div id="header">
-      <Home />
+      <Home/>
       </div>
+      
       <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/" element={<AppointmentList />} />
         <Route path="/create" element={<AddAppointment />} />
         <Route path="/completed" element={<CompletedAppointments />} />
         <Route path="/update" element={<UpdateAppointment />} />  
       </Routes>
+     
     </div>
 );
+
+
 
 
 export default App;
